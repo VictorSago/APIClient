93d77d007ab8a7fa3d931dbf34e629df9441585d
Victor Sago <victor.sago@elstandard.se>
2022-12-15 19:56:25 +0100

    Added docstrings and comments

--- run.py
+++ run.py
@@ -20,19 +20,36 @@ endpoints = api_endpoints.endpoints
 
 
 def get_auth(baseurl, auth_path, api_key, api_secret):
+    """
+    Get access token to be used in subsequent calls
+    """
     auth_url = baseurl + auth_path
     resp = requests.get(auth_url, auth=(api_key, api_secret))
     return resp
 
 
 def get_access_token(baseurl=BASE_URL, auth_path=AUTH_PATH, api_key=API_KEY, api_secret=API_SECRET):
+    """
+    Retrieve the access token
+    The function makes a call to the API and then extracts the access token from the response.
+    :param baseurl: Base URL
+    :param auth_path: Path at the API for retrieving the access authorization
+    :param api_key: API Key
+    :param api_secret: API Secret
+    :return: Access token
+    """
     resp = get_auth(baseurl, auth_path, api_key, api_secret)
     resp_json = resp.json()
     return resp_json["access_token"]
 
 
-def save_to_file(data, name):
-    filepath = "retrieved_data/" + name
+def save_to_file(data, filename):
+    """
+    Save data to file
+    :param data: The data in the json format
+    :param filename: The name of the file
+    """
+    filepath = "retrieved_data/" + filename
     with open(filepath, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
@@ -46,8 +63,10 @@ def main():
             for endpoint in nfo["APIEndpoints"]:
                 endpoint_url = f"{nfo['BaseURL'] + nfo['APIPath']}/{endpoint}"
                 api_resp = s.get(endpoint_url)
-                filename = f"{org}-{endpoint}-{datetime.now().strftime('%Y%m%d_%H%M')}.json"
-                save_to_file(api_resp.json(), filename)
+                # The retrieved data is saved in a file with a name that combines the name of 
+                # the organization, the type of data retrieved and today's date and time
+                file_name = f"{org}-{endpoint}-{datetime.now().strftime('%Y%m%d_%H%M')}.json"
+                save_to_file(api_resp.json(), file_name)
 
 
 if __name__ == "__main__":
